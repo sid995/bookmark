6d6c51675a8d8d90e34813bad7fff93492909b42
Siddharth Kundu <sk31395@gmail.com>
2023-05-05 19:13:09 +0530

    smart redirect for twitter and other sites

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+use rocket::response::Redirect;
+mod utils;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/search?<cmd>")]
+fn search(cmd: String) -> Redirect {
+    let command = utils::get_command_from_query_string(&cmd);
+
+    let redirect_url = match command {
+        "tw" => utils::twitter::construct_twitter_url(&cmd),
+        _ => utils::google::construct_google_search_url(&cmd),
+    };
+    Redirect::to(redirect_url)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, search])
 }
